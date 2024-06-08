73df46a399f542a9d89d149831a9c2a9699e02f7
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-06-08 14:36:29 +0500

    Time: 31 ms (77.76%), Space: 16.4 MB (74.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # Recursive Approach:
-        if n <= 2:
-            return n
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        # if n <= 2:
+        #     return n
+        # return self.climbStairs(n - 1) + self.climbStairs(n - 2)
 
 
         # Tabulation (Bottom-up Dynamic Programming):
-        # if n <= 2:
-        #     return n
-        # dp = [0] * (n + 1)
-        # dp[1] = 1
-        # dp[2] = 2
-        # for i in range(3, n + 1):
-        #     dp[i] = dp[i - 1] + dp[i - 2]
-        # return dp[n]
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
 
 
         # Fibonacci Series (Optimal)
