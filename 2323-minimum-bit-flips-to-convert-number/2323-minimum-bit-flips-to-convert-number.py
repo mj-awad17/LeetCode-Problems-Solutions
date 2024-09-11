a0b03ad3fe5e0aa6f2a344bd3be2fdb75239b783
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-09-11 21:57:43 +0500

    Time: 29 ms (89.67%), Space: 16.5 MB (69.15%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         # method 1
-        res = 0
+        """res = 0
         while start or goal:
             if (start % 2) != (goal % 2):
                 res += 1
             start =  start >> 1
             goal  =  goal >> 1
-
+        return res"""
+        # method 2 - XOR
+        res = 0
+        n = start ^ goal
+        while n:
+            res += n & 1
+            n = n >> 1
         return res
