77bc6d7c83bf31ab7e3a7aace04bbf4efe435a63
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-10-05 00:44:02 +0500

    Time: 109 ms (94.42%), Space: 12.1 MB (78.49%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,15 +1,21 @@
 class Solution(object):
     def canPlaceFlowers(self, flowerbed, n):
+        # count the flowers that are placed
         count = 0
         size = len(flowerbed)
         i = 0
         while i < size:
+            # the current spot is empty
             if flowerbed[i] == 0:
+                # Check the last spot or the next spot is also empty
                 if i == size - 1 or flowerbed[i+1] == 0:
                     count += 1
+                    # move to next spot after one
                     i += 2
                 else:
+                    # move one sopt next
                     i += 1
             else:
+                # skip if flowered ploted
                 i += 2
         return count >= n
