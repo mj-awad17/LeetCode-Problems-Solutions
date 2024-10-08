efc3069084b64da13e6338778f91c2e96b313060
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-10-08 22:45:42 +0500

    Time: 826 ms (70.89%), Space: 25.3 MB (99.83%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,7 +1,13 @@
 class Solution(object):
     def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Initialize the 1st & 2nd smallest number to positive infinity
         _1st = ('inf')
         _2nd = ('inf')
+        # iterate through list
         for num in nums:
             if num <= _1st:
                 _1st = num
