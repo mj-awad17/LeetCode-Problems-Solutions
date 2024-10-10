78544923efba978843b0dff035c642c26bfdfcd9
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-10-10 22:09:48 +0500

    Time: 38 ms (55.33%), Space: 16.5 MB (22.71%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,11 +1,13 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-
+        # Initialize the maximum altitude and current altitude
         maxAltitude = 0
         currAltitude = 0
-
+        # Iterate through each gain value in the list
         for g in gain:
+            # the current altitude by adding the current gain
             currAltitude += g
+            # Update the maximum altitude if the current altitude is greater
             maxAltitude = max(maxAltitude, currAltitude)
 
         return maxAltitude
