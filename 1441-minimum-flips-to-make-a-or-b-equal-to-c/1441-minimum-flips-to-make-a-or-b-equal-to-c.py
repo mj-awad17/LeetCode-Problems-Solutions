2196192fa5e6f79b616c5734e5257de758b565a6
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-10-18 22:58:14 +0500

    Time: 38 ms (30.36%), Space: 16.4 MB (85.32%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,13 +1,13 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
         count = 0
-        
+        # >> = right most shift, '&' and operator
         for i in range(32):  # Check each bit position
             bit_a = (a >> i) & 1
             bit_b = (b >> i) & 1
             bit_c = (c >> i) & 1
             
-            # Check if (bit_a | bit_b) matches bit_c
+            # Check if OR operator holds by -> (bit_a | bit_b) matches bit_c
             if (bit_a | bit_b) != bit_c:
                 if bit_c == 1:
                     # If bit_c is 1, we need at least one of bit_a or bit_b to be 1
