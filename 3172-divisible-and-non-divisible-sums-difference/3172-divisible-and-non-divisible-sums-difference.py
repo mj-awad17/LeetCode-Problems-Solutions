8c8572727e34e1059d21d58fc75acc99103863e3
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2025-06-05 15:28:26 +0500

    Time: 3 ms (49.38%), Space: 17.8 MB (67.05%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
@@ -7,10 +7,9 @@ class Solution:
         # nums range to n
         for i in range(1, n+1):
             # fall conditions
-            if i % m == 0:
+            if i % m == 0: # != 
                 num2 += i
             else:
                 num1 += i
-            pass
 
         return num1 - num2
